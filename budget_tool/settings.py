49d1105e17cd12e6d33dea4da29fb7820781a4c0
mthwbrwn <mthwbrwn@gmail.com>
2019-01-21 21:32:52 -0800

    added models and admin

--- budget_tool/settings.py
+++ budget_tool/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'budget_app'
+    'budget_tool'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
