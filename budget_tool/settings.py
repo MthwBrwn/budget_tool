215790d0f1ec3e2c5da94fd391bbd5729221f8af
mthwbrwn <mthwbrwn@gmail.com>
2019-01-24 21:42:39 -0800

    changes made to model

--- budget_tool/settings.py
+++ budget_tool/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split()
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
 
 
 # Application definition
@@ -127,7 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Django Registration Settings
 ACCOUNT_ACTIVATION_DAYS = 1
--- budget_tool/urls.py
+++ budget_tool/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from .views import home_view
 from django.urls import path
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 
